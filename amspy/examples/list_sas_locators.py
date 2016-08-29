71423eab494f89ac2a97a5f401ec6f01efd845d4
Marcelo Leal <msl@eall.com.br>
2016-08-29 15:10:42 +0000

    Added the list_sas_locators.py example

---
+++ amspy/examples/list_sas_locators.py
@@ -0,0 +1,59 @@
+"""
+Copyright (c) 2016, Marcelo Leal
+Description: Simple Azure Media Services Python library
+License: MIT (see LICENSE.txt file for details)
+"""
+import os
+import json
+import amspy
+import time
+#import pytz
+import logging
+import datetime
+
+# Load Azure app defaults
+try:
+	with open('config.json') as configFile:
+		configData = json.load(configFile)
+except FileNotFoundError:
+	print("ERROR: Expecting config.json in current folder")
+	sys.exit()
+
+subscription_id = configData['subscriptionId']
+rg_name = configData['rgName']
+account_name = configData['accountName']
+account_key = configData['accountKey']
+log_name = configData['logName']
+log_level = configData['logLevel']
+purge_log = configData['purgeLog']
+time_zone = configData['timeZone']
+region = configData['region']
+
+#Remove old log file if requested (default behavior)...
+if (purge_log.lower() == "yes"):
+        if (os.path.isfile(log_name)):
+                os.remove(log_name);
+
+#Basic Logging...
+logging.basicConfig(format='%(asctime)s - %(levelname)s:%(message)s', level=log_level, filename=log_name)
+
+# Get the access token...
+response = amspy.get_access_token(account_name, account_key)
+resjson = response.json()
+access_token = resjson["access_token"]
+
+#Initialization...
+print ("\n-----------------------= AMS Py =----------------------");
+print ("Simple Python Library for Azure Media Services REST API");
+print ("-------------------------------------------------------\n");
+
+### list the sas locator
+print ("\n008 >>> Listing a SAS Locator")
+response = amspy.list_sas_locator(access_token)
+if (response.status_code == 200):
+        resjson = response.json()
+        print("GET Status..............................: " + str(response.status_code))
+        for sl in resjson['d']['results']:
+                print("SAS Locator Id..........................: " + str(sl['Id']))
+else:
+        print("GET Status..............................: " + str(response.status_code) + " - SAS Locator List ERROR." + str(response.content))
